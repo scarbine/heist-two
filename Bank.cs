d119a86eb9c09edfebb50e38edefb67064be546c
Sam Carbine <scarbine@me.com>
2021-09-07 10:18:38 -0500

    first Commit

---
+++ Bank.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace heist_two
+{
+    public class Bank
+    {
+        public int CashOnHand {get; set;}
+
+        public int AlarmScore {get;set;}
+
+        public int VaultScore {get; set;}
+
+        public int SecurityGuardScore {get; set;}
+
+        public bool IsSecure {get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace heist_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
